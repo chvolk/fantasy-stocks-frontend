b66fe6c33d2a6ccab1c92edc7aefb460dbe1451c
Chambers Volk <chambersvolk@outlook.com>
2024-09-27 10:53:07 -0700

    moq draft

--- src/components/StockDraft.vue
+++ src/components/StockDraft.vue
@@ -4,7 +4,7 @@
       <v-col cols="12" md="10" lg="8">
         <v-card elevation="12" class="pa-6">
           <v-card-title class="text-h4 font-weight-bold text-center mb-4">
-            Stock Draft
+            Moq Draft
           </v-card-title>
           
           <v-card-subtitle class="text-h5 text-center mb-4">
