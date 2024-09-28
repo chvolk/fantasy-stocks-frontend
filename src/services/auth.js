d7c49ec030cc1f74b54fa152c7187de7f37f661e
Chambers Volk <chambersvolk@outlook.com>
2024-09-28 11:01:10 -0700

    asdf

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,10 +3,9 @@ import api from './api';
 const authService = {
   async login(username, password) {
     try {
-      console.log('Attempting to login with:', { username, password });
       const response = await api.post('/login/', { username, password });
-      console.log('Login response:', response);
       const token = response.data.token;
+      console.log('Received token:', token); // Debug log
       localStorage.setItem('token', token);
       return response.data;
     } catch (error) {
@@ -15,17 +14,8 @@ const authService = {
       throw error;
     }
   },
-  async signup(user) {
-    try {
-      console.log('Attempting to signup with:', user);
-      const response = await api.post('/signup/', user);
-      console.log('Signup response:', response);
-      return response.data;
-    } catch (error) {
-      console.error('Signup error:', error);
-      this.logAxiosError(error);
-      throw error;
-    }
+  signup(user) {
+    return api.post('/signup/', user);
   },
   logAxiosError(error) {
     if (error.response) {
