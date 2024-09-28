5781b4bfd08cced22298e557748e088e81b14344
Chambers Volk <chambersvolk@outlook.com>
2024-09-28 10:36:00 -0700

    aasd

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import store from './store'  // Import your store
 
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App)
+  .use(store)  // Use the store
+  .use(vuetify)
+  .mount('#app')
