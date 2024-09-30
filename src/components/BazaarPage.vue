adc39a419951a115ab7a319936ebd596f0f23d7b
Chambers Volk <chambersvolk@outlook.com>
2024-09-30 16:25:02 -0700

    fix

--- src/components/BazaarPage.vue
+++ src/components/BazaarPage.vue
@@ -233,7 +233,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="packDialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" text @click="packDialog = false" :disabled="packOpeningState !== 'revealed'">Close</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
